3a47a0eee00af2ced77a410ed531af51919a2551
Name <user@example.com>
2021-03-16 10:26:52 -0400

    bbb

--- all_checks.py
+++ all_checks.py
@@ -19,6 +19,7 @@ def check_disk_full(disk, min_gb, min_percent):
         return True
     return False
 
+
 #Check for at least 2 GB and 10% free
 if not check_reboot():
     print("Pending Reboot.")
@@ -26,8 +27,21 @@ if not check_reboot():
 if not check_disk_full(disk="/", min_gb=2, min_percent=10):
     print("Disk full.")
     sys.exit(1)
+def main():
+    checks=[
+        (check_reboot, "Pending Reboot"),
+        (check_root_full, "Root partition full"),
+    ]
+    everything_ok= True
+    for check, msg in checks:
+        if check():
+            print(msg)
+            everything_ok= False
+            
+    if not everything_ok:
+        sys.exit(1)         
+    
+    print("Everything ok.")
+    sys.exit(0)
 
-print("Everything ok.")
-sys.exit(0)
-print("master chenge")
-print("master chenge2")
+main()
